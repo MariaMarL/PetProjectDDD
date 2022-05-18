8895dae6e477292494813bb81f2fd61666f10726
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-05-18 09:38:01 -0500

    initializing project with spring initilizr

---
+++ src/test/java/com/sofkaU/demoDDD/DemoDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.demoDDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
